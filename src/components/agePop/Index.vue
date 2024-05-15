6b64465b0a12afb5ad94faac2f2e11131d34ef43
30table <dev@30table.com>
2024-05-15 10:57:27 +0800

    update

--- src/components/agePop/Index.vue
+++ src/components/agePop/Index.vue
@@ -1,43 +1,14 @@
 <template>
-  <div
-    id="agePop"
-    class="pb-[10vh] pop inset-0 z-50 fixed bg-linear-1 flex justify-center items-center"
-  >
-    <div
-      class="absolute inset-0 z-0"
-      :style="{ backgroundImage: `url(${getUrl('wave-r.png')})` }"
-    ></div>
-    <div
-      class="age-content w-full max-w-2xl space-y-8 conatiner flex flex-col justify-center items-center text-gold md:px-8 md:w-full relative z-10"
-    >
+  <div id="agePop" class="pb-[10vh] pop inset-0 z-50 fixed bg-linear-1 flex justify-center items-center">
+    <div class="absolute inset-0 z-0" :style="{ backgroundImage: `url(${getUrl('wave-r.png')})` }"></div>
+    <div class="age-content w-full max-w-2xl space-y-8 conatiner flex flex-col justify-center items-center text-gold md:px-8 md:w-full relative z-10">
       <div class="relative max-w-md mb-20">
-        <img
-          :src="getUrl(`title0${index}.png`)"
-          v-for="index in 8"
-          :key="index"
-          class="inset-0 ani-logo"
-          :class="[
-            index === 1 ? 'relative' : 'absolute',
-            index === 8 ? 'mt-20' : '',
-          ]"
-        />
+        <img :src="getUrl(`title0${index}.png`)" v-for="index in 8" :key="index" class="inset-0 ani-logo" :class="[index === 1 ? 'relative' : 'absolute', index === 8 ? 'mt-20' : '']" />
       </div>
       <div class="tracking-wider text-xl ani-f-b">請先確認您是否已滿18歲</div>
       <div class="flex w-full space-x-8 ani-f-b">
-        <div
-          data-g="18歲年齡檢查否 "
-          class="gBtn border btn border-gold no-btn text-center py-3 cursor-pointer duration-300 hover:text-white grow"
-          @click="popAlert()"
-        >
-          否
-        </div>
-        <div
-          data-g="18歲年齡檢查_是"
-          class="gBtn border border-gold btn yes-btn text-center py-3 cursor-pointer duration-300 hover:text-white grow"
-          @click="onYes"
-        >
-          是
-        </div>
+        <div data-g="18歲年齡檢查否 " class="gBtn border btn border-gold no-btn text-center py-3 cursor-pointer duration-300 hover:text-white grow" @click="popAlert()">否</div>
+        <div data-g="18歲年齡檢查_是" class="gBtn border border-gold btn yes-btn text-center py-3 cursor-pointer duration-300 hover:text-white grow" @click="onYes">是111</div>
       </div>
     </div>
   </div>
